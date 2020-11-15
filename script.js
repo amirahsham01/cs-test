3833917861df384cec6f6b3375553351a7d76934
Nur Amirah Sham <nuramirahsham@gmail.com>
2020-11-15 23:08:11 +0800

    completed template for responsiveness

---
+++ script.js
@@ -0,0 +1,24 @@
+// const mql = window.matchMedia('(min-width: 768px)');
+
+// let handleMatchMedia = function(mediaQuery) {
+//     if (mediaQuery.matches) {
+//         $(".content__section4").children().removeClass("col-3");
+//         $(".content__section4").children().addClass("col-6");
+//     } else {
+//         $(".content__section4").children().addClass("col-3");
+//         $(".content__section4").children().removeClass("col-6");
+//     }
+// };
+
+// handleMatchMedia(mql);
+
+// mql.addListener(handleMatchMedia);
+// const section4 = document.querySelectorAll(".content__section4");
+// const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+// mediaQuery.addEventListener("change", () => {
+//     if (mediaQuery.matches) {
+//         section4.classList.remove("col-3");
+//         section4.classList.add("col-6");
+//     }
+// });
